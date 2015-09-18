756a2ca928f4475a0464b8ee42946e7d2fefb7f4
Zee Xiao <zxiao@originconsults.com>
2015-09-18 21:04:53 +0100

    layout

--- db/schema.rb
+++ db/schema.rb
@@ -14,84 +14,66 @@
 ActiveRecord::Schema.define(version: 20150913180056) do
 
   create_table "projects", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.text     "description", limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "projects_users", id: false, force: :cascade do |t|
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.integer  "user_id",    limit: 4
-    t.integer  "project_id", limit: 4
-    t.integer  "role_id",    limit: 4
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.integer  "role_id"
   end
 
-  add_index "projects_users", ["project_id"], name: "fk_rails_1e950bec4e", using: :btree
-  add_index "projects_users", ["role_id"], name: "fk_rails_a1ee7876a0", using: :btree
-  add_index "projects_users", ["user_id"], name: "fk_rails_0a98a1147c", using: :btree
-
   create_table "roles", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "roles_users", id: false, force: :cascade do |t|
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.integer  "user_id",    limit: 4
-    t.integer  "role_id",    limit: 4
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "role_id"
   end
 
-  add_index "roles_users", ["role_id"], name: "fk_rails_9dada905f6", using: :btree
-  add_index "roles_users", ["user_id"], name: "fk_rails_e2a7142459", using: :btree
-
   create_table "tasks", force: :cascade do |t|
-    t.string   "name",             limit: 255
-    t.integer  "project_id",       limit: 4
-    t.integer  "created_by_user",  limit: 4
-    t.integer  "assigned_to_user", limit: 4
-    t.integer  "estimated_hours",  limit: 4
+    t.string   "name"
+    t.integer  "project_id"
+    t.integer  "created_by_user"
+    t.integer  "assigned_to_user"
+    t.integer  "estimated_hours"
     t.date     "creation_date"
     t.date     "completion_date"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.integer  "status",           limit: 4
-    t.integer  "priority",         limit: 4
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "status"
+    t.integer  "priority"
     t.date     "due_date"
   end
 
-  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id", using: :btree
+  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id"
 
   create_table "user_task_logs", id: false, force: :cascade do |t|
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.date     "date"
-    t.integer  "hours",      limit: 4
-    t.integer  "user_id",    limit: 4
-    t.integer  "task_id",    limit: 4
+    t.integer  "hours"
+    t.integer  "user_id"
+    t.integer  "task_id"
   end
-
-  add_index "user_task_logs", ["task_id"], name: "fk_rails_fdd50066bf", using: :btree
-  add_index "user_task_logs", ["user_id"], name: "fk_rails_66a314531c", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",   limit: 255
-    t.string   "password",   limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.string   "first_name", limit: 255
-    t.string   "last_name",  limit: 255
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
-  add_foreign_key "projects_users", "projects"
-  add_foreign_key "projects_users", "roles"
-  add_foreign_key "projects_users", "users"
-  add_foreign_key "roles_users", "roles"
-  add_foreign_key "roles_users", "users"
-  add_foreign_key "tasks", "projects"
-  add_foreign_key "user_task_logs", "tasks"
-  add_foreign_key "user_task_logs", "users"
 end
